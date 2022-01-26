68f85c97fbf82591654a346b4e239aa57ba70281
Joseph Miller <josephmiller@Josephs-MBP.attlocal.net>
2022-01-26 14:16:46 -0500

    db config for heroku

--- backend/SAFProj/SAFProj/settings.py
+++ backend/SAFProj/SAFProj/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -80,7 +82,7 @@ WSGI_APPLICATION = 'SAFProj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -90,8 +92,11 @@ DATABASES = {
         'HOST': 'localhost',
     }
 }
+"""
 
-
+DATABASES = { 
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
